ca1d53cdc4c1358e0c033795f26b82a86c1157d7
Satrio Wisnugroho <wisnu@skyshi.com>
2018-01-18 10:14:47 +0700

    Change language

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const request = require('request');
 require('dotenv').config();
 
-const url = `http://sandbox.api.simsimi.com/request.p?key=${process.env.SIMSIMI_KEY}&lc=en&ft=1.0&text=`;
+const url = `http://sandbox.api.simsimi.com/request.p?key=${process.env.SIMSIMI_KEY}&lc=id&ft=1.0&text=`;
 
 const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
 
