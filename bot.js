72b2a84095f4680689580f30cbbab460eecea9cc
nonknet <nonknet@gmail.com>
2018-05-10 14:11:38 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
-const TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require('536596192:AAEkE0hSpim7eELbZIgAAWZInQiP349ycvU');
 const request = require('request');
 require('dotenv').config();
 
-const url = `http://sandbox.api.simsimi.com/request.p?key=${process.env.SIMSIMI_KEY}&lc=id&ft=1.0&text=`;
+const url = `http://sandbox.api.simsimi.com/request.p?key=$db6a90c1-5e62-46f1-8cd0-7f44f364480a&lc=id&ft=1.0&text=`;
 
-const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
+const bot = new TelegramBot536596192:AAEkE0hSpim7eELbZIgAAWZInQiP349ycvU, { polling: true });
 
 bot.on('message', (msg) => {
     const chatId = msg.chat.id;
